a8b0880ecde98d702feff11743690bea1433374d
Chiththaja <162040367+ChiththajaGalabodage@users.noreply.github.com>
2025-10-10 10:19:31 +0530

    n

--- src/components/imageSlider.jsx
+++ src/components/imageSlider.jsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
 export default function ImageSlider(props) {
-  const image = props.image;
+  const I = props.images;
+  const [currentIndex, setCurrentIndex] = useState(0);
   return (
     <div className="w-[90%] md:w-[500px] h-[600px]  bg-amber-50  ">
-      <img className="w-full h-[500px]" />
+      <img src={I[currentIndex]} className="w-full h-[500px] object-cover" />
       <div className="w-full h-[100px] bg-slate-300 flex justify-center items-center">
-        {image?.map((image, index) => {
+        {I?.map((image, index) => {
           return (
-            <img key={index} className="w-[90px] h-[90px] mx-2" src={image} />
+            <img
+              key={index}
+              className={
+                "w-[90px] h-[90px] mx-2 rounded-2xl object-cover cursor-pointer hover:border-4 hover:border-blue-900" +
+                (index == currentIndex && "border-accent border-4")
+              }
+              src={image}
+              onClick={() => setCurrentIndex(index)}
+            />
           );
         })}
       </div>
